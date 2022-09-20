0c393605130a9f5d30289aec53b4ceb5c4153cd6
AlifiyulAkyun <alifiyulakyun1@gmail.com>
2022-09-20 19:02:16 +0700

    Menambahkan Tugas Kedua

---
+++ HelloConstraint/app/src/main/java/jti/AlifiyulAkyun06/com/MainActivity.java
@@ -0,0 +1,36 @@
+package jti.AlifiyulAkyun06.com;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void showZero(View view) {
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+}
